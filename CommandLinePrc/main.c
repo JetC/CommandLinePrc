0e54fc8135a2253aa8a56d76f90fa1505bdf0eaa
Sun PeiFeng <g7733pf@gmail.com>
2015-01-15 20:00:53 +0800

    Initial Commit

---
+++ CommandLinePrc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CommandLinePrc
+//
+//  Created by 孙培峰 on 1/15/15.
+//  Copyright (c) 2015 孙培峰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
